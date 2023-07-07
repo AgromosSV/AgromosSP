b8d6d797feb83a13e74f0a91291c16acda9c3e8a
Agromos <138736712+AgromosSV@users.noreply.github.com>
2023-07-07 00:31:39 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 //============= Datos del Bot =============//
 
-global.owner = ['59894808483', '5214531106422', '5219996125657', '5492266466080']
+global.owner = ['595983186566', '0000000', '00000007', '02520525']
 global.packname = '(☞ﾟ∀ﾟ)☞'
-global.author = '𝕊𝕚𝕞𝕡𝕝𝕖𝔹𝕠𝕥'
-global.wm = '𝕊𝕚𝕞𝕡𝕝𝕖𝔹𝕠𝕥'
+global.author = 'AGROMOS BOT'
+global.wm = 'AGROMOS BOT'
 global.wait = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
 
 //============= urlButtons =============//
 
 global.textUrlbot = 'ɢɪᴛʜᴜʙ'
-global.linkUrlbot = "https://github.com/BrunoSobrino/SimpleBot"
+global.linkUrlbot = "https://github.com/AgromosSV/AgromosSP"
 
 //=========================================================================//
 
@@ -29,7 +29,7 @@ global.APIKeys = {
 'https://api.xteam.xyz': 'd90a9e986e18778b',
 'https://zahirr-web.herokuapp.com': 'zahirgans' 
 }
-global.lolkeysapi = ['BrunoSobrino']
+global.lolkeysapi = ['AGROMOS']
 global.keysZens = ['fiktod', 'c2459db922', 'BF39D349845E', '675e34de8a', '37CC845916', '0b917b905e6f', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.multiplier = 9999 
